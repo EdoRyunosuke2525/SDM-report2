517f1729e0b0d042542e5d7a6806a0bde7908aa1
EdoRyunosuke2525 <99185011+EdoRyunosuke2525@users.noreply.github.com>
2025-01-30 08:45:48 +0900

    Add test cases and fix calc() function

--- test_cases.py
+++ test_cases.py
@@ -3,19 +3,22 @@
 import unittest
 from calc_mul import calc
 
-# Run with testrunner so needs to be in file test_
+class TestCalc(unittest.TestCase):
 
-class TestCalc (unittest.TestCase):
+    def test_valid_inputs(self):
+        self.assertEqual(calc(1, 1), 1)  # 最小境界値
+        self.assertEqual(calc(999, 999), 998001)  # 最大境界値
+        self.assertEqual(calc(500, 500), 250000)  # 中央値
 
-        def test_sample1 (self):
-                self.assertEqual (21, calc(3,7))
-
-        def test_sample2 (self):
-                self.assertEqual (-1, calc(0,150))
-
-        def test_sample3 (self):
-                self.assertEqual (-1, calc('a','b'))
-
-        def test_sample4 (self):
-                self.assertEqual (-1, calc(0.1,999))
-
+    def test_invalid_inputs(self):
+        self.assertEqual(calc(0, 500), -1)  # 境界値以下
+        self.assertEqual(calc(1000, 500), -1)  # 境界値以上
+        self.assertEqual(calc(500, 0), -1)  # 境界値以下
+        self.assertEqual(calc(500, 1000), -1)  # 境界値以上
+        self.assertEqual(calc(-1, 500), -1)  # 負の数
+        self.assertEqual(calc(500, -1), -1)  # 負の数
+        self.assertEqual(calc("a", 500), -1)  # 文字列
+        self.assertEqual(calc(500, "b"), -1)  # 文字列
+        self.assertEqual(calc(0.5, 500), -1)  # 小数
+        self.assertEqual(calc(500, 0.9), -1)  # 小数
+        
